2c4166f826e88780011370c861b4802d974abeca
Yuchen Wang <phobosw@gmail.com>
2014-05-03 02:45:36 +0800

    add window skipping to increase speed

--- bpm.py
+++ bpm.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import argparse
+import random
 import wave
 
 import numpy
@@ -52,7 +53,8 @@ def select_bpm(blist, tolerance=3, verbose=False):
     return final_bpm, final_proportion
 
 
-def detect_wav(filename, window=3, overlap=0, rmvocal=False, lowpass=False, down_factor=1, verbose=False):
+def detect_wav(filename, window=3, overlap=0, rmvocal=False, lowpass=False,
+               down_factor=1, skip_window=0, verbose=False):
     wf = wave.open(filename, 'rb')
     nchannels, sampwidth, framerate, nframes = wf.getparams()[:4]
 
@@ -63,10 +65,15 @@ def detect_wav(filename, window=3, overlap=0, rmvocal=False, lowpass=False, down
         f = scipy.signal.firwin(numtaps=20, cutoff=8, nyq=20)
 
     blist = []
-    step = window * framerate
-    for x in range(0, nframes, step):
+    window_nframes = window * framerate
+    window_count = nframes / window_nframes
+    for window_id in range(0, window_count):
+        # Skip window
+        if skip_window > 0 and random.randrange(skip_window) == 0:
+            continue
+
         # Read window then convert
-        wdata = numpy.fromstring(wf.readframes(step), dtype='<i%d' % sampwidth)
+        wdata = numpy.fromstring(wf.readframes(window_nframes), dtype='<i%d' % sampwidth)
 
         # Mix into one channel
         if rmvocal and nchannels == 2:
@@ -77,7 +84,7 @@ def detect_wav(filename, window=3, overlap=0, rmvocal=False, lowpass=False, down
             wdata = wdata[::nchannels]
 
         # Break if sample too short
-        if len(wdata) < step:
+        if len(wdata) < window_nframes:
             break
 
         # Overlap
@@ -97,12 +104,12 @@ def detect_wav(filename, window=3, overlap=0, rmvocal=False, lowpass=False, down
         try:
             bpm = detect(wdata, framerate / down_factor)
         except ValueError as e:
-            print '%5.1f%% error: %s' % (100. * x / nframes, e)
+            print '%5.1f%% error: %s' % (100. * window_id / window_count, e)
             continue
         else:
             blist.append(bpm)
             if verbose:
-                print '%5.1f%% %d' % (100. * x / nframes, bpm)
+                print '%5.1f%% %d' % (100. * window_id / window_count, bpm)
 
     wf.close()
     return blist
@@ -119,6 +126,7 @@ if __name__ == '__main__':
     parser.add_argument('--rmvocal', action='store_true', help='remove vocal by mixing left with inverted right channel. Default: False')
     parser.add_argument('--lowpass', action='store_true', help='use Low Pass Filter to cut treble sound. Default: False')
     parser.add_argument('--downsample', type=int, default=1, metavar='N', help='downsample by an integer factor that will increase speed. Default: 1')
+    parser.add_argument('--skipwindow', type=int, default=0, metavar='N', help='skip window detecting every N times. Default: 0')
     # Select
     parser.add_argument('--tolerance', '-t', type=int, default=3, metavar='N', help='tolerance using in select final BPM. Default: 3')
     args = parser.parse_args()
@@ -136,6 +144,7 @@ if __name__ == '__main__':
             'rmvocal': args.rmvocal,
             'lowpass': args.lowpass,
             'down_factor': args.downsample,
+            'skip_window': args.skipwindow,
             'verbose': args.verbose,
         }
         blist = detect_wav(filename, **options)
