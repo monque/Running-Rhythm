2aa63c6b2d70fafa94dbf453624b8bc90de41b15
Yuchen Wang <phobosw@gmail.com>
2014-05-02 22:57:04 +0800

    refactor select_bpm function rename invertmix to remove vocal

--- bpm.py
+++ bpm.py
@@ -12,21 +12,25 @@ BPM_MAX = 200
 BTABLE = {x: 0 for x in range(BPM_MIN, BPM_MAX + 1)}
 
 
-def blist_analyze(blist, tolerance=3, verbose=False):
+def select_bpm(blist, tolerance=3, verbose=False):
     if not blist:
-        raise ValueError('Empty BPM list')
+        raise ValueError('empty BPM list')
 
-    # Convert to dict
+    # Duplicate with multiple
+    blen = len(blist)
+    for bpm in blist[:]:
+        blist.append(bpm * 2)
+        blist.append(bpm / 2)
+
+    # Convert to table
     btable = BTABLE.copy()
     for bpm in blist:
-        for b in [bpm, bpm * 2, bpm / 2]:  # Multiple
-            if b in btable:
-                btable[b] += 1
+        if bpm in btable:
+            btable[bpm] += 1
 
-    # Calculate weight
     final_bpm = final_weight = 0
     for bpm in sorted(btable.keys()):
-        # Tolerance
+        # Calculate weight
         wlist = []
         for b in range(bpm - tolerance, bpm + tolerance + 1):
             if b not in btable:
@@ -35,51 +39,47 @@ def blist_analyze(blist, tolerance=3, verbose=False):
             wlist.append(factor * btable[b])
         weight = sum(wlist)
 
-        if verbose and weight > 0:
-            print '%3d %5.2f %s' % (bpm, weight, wlist)
-
+        # Set final
         if weight > final_weight:
             final_bpm = bpm
             final_weight = weight
 
-    final_weightp = 100. * final_weight / len(blist)
-    return final_bpm, int(final_weightp)
+        if verbose and weight > 0:
+            print '%3d %5.2f %s' % (bpm, weight, wlist)
+
+    final_proportion = int(100 * final_weight / blen)
+    return final_bpm, final_proportion
 
 
-def detect_wav(filename, window=3, downsample=1, invertmix=False, verbose=False):
+def detect_wav(filename, window=3, down_factor=1, rmvocal=False, verbose=False):
     wf = wave.open(filename, 'rb')
     nchannels, sampwidth, framerate, nframes = wf.getparams()[:4]
 
-    # Data type
-    if sampwidth == 2:
-        dtype  = '<h'
-    elif sampwidth == 4:
-        dtype  = '<i'
-
-    # Process by window
     blist = []
     step = window * framerate
     for x in range(0, nframes, step):
-        wdata = numpy.fromstring(wf.readframes(step), dtype=dtype)
-        if invertmix and nchannels == 2:  # try to remove vocal
-            left = wdata[::2]
-            right = wdata[1::2]
-            wdata = (left - right) / 2
-        else:
-            wdata = wdata[::nchannels]  # only detect first channel
+        # Read window then convert
+        wdata = numpy.fromstring(wf.readframes(step), dtype='<i%d' % sampwidth)
 
-        # Break when wdata too short
+        # Extract sample
+        if rmvocal and nchannels == 2:
+            # remove vocal by mix invert channel
+            wdata = (wdata[::2] - wdata[1::2]) / 2
+        else:
+            # only detect first channel
+            wdata = wdata[::nchannels]
+
+        # Break when sample too short
         if len(wdata) < step:
             break
 
         # Downsample
-        if downsample > 1:
-            # wdata = scipy.signal.decimate(wdata, downsample)
-            wdata = wdata[::downsample]
+        if down_factor > 1:
+            wdata = wdata[::down_factor]
 
         # Detect
         try:
-            bpm = detect(wdata, framerate / downsample)
+            bpm = detect(wdata, framerate / down_factor)
         except ValueError as e:
             print '%5.1f%% error: %s' % (100. * x / nframes, e)
             continue
@@ -93,22 +93,27 @@ def detect_wav(filename, window=3, downsample=1, invertmix=False, verbose=False)
 
 
 if __name__ == '__main__':
-    # Arguments
+    # Argument
     parser = argparse.ArgumentParser(description='Process .wav file to determine the Beats Per Minute.')
     parser.add_argument('file', type=str, nargs='+',
         help='.wav file for processing')
+    parser.add_argument('--verbose', '-v', action='store_true')
+
+    # Detect
     parser.add_argument('--window', '-w', type=int, default=3, metavar='S',
         help='size of the the window (seconds) that will be scanned to determine the bpm. Default: 3')
-    parser.add_argument('--tolerance', '-t', type=int, default=3, metavar='N',
-        help='tolerance using in window BPM result analyze. Default: 3')
-    parser.add_argument('--invertmix', action='store_true',
-        help='[EXPERIMENTAL] mix left channel with inverted right channel, it will remove vocal to improve detect. Default: False')
+    parser.add_argument('--rmvocal', action='store_true',
+        help='[EXPERIMENTAL] try to remove vocal by mixing left channel with inverted right channel. Default: False')
     parser.add_argument('--downsample', type=int, default=1, metavar='Q',
-        help='[EXPERIMENTAL] downsample to RATE to improve speed. Default: 1')
-    parser.add_argument('--verbose', '-v', action='store_true')
+        help='[EXPERIMENTAL] downsample by an integer factor that will increase speed. Default: 1')
+
+    # Select
+    parser.add_argument('--tolerance', '-t', type=int, default=3, metavar='N',
+        help='tolerance using in select final BPM. Default: 3')
+
     args = parser.parse_args()
 
-    # Loop file list
+    # Process file list
     op_showfile = len(args.file) > 1
     for filename in args.file:
         if args.verbose:
@@ -116,13 +121,13 @@ if __name__ == '__main__':
 
         # Detect
         blist = detect_wav(filename, args.window, args.downsample, False, args.verbose)
-        if args.invertmix:
+        if args.rmvocal:
             blist += detect_wav(filename, args.window, args.downsample, True, args.verbose)
 
-        # Analyze
-        bpm, weightp = blist_analyze(blist, args.tolerance, args.verbose)
+        # Select
+        bpm, proportion = select_bpm(blist, args.tolerance, args.verbose)
 
         if op_showfile:
-            print '%3d %3d %s' % (bpm, weightp, filename)
+            print '%3d %3d %s' % (bpm, proportion, filename)
         else:
-            print '%3d %3d' % (bpm, weightp)
+            print '%3d %3d' % (bpm, proportion)
